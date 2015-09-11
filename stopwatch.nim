a76b86d5b70338cc38aba6c8c1e5eae8ca72ae88
Huy ☭ <rgv151@users.noreply.github.com>
2015-09-11 09:30:51 +0700

    Clean and simpler version

--- stopwatch.nim
+++ stopwatch.nim
@@ -12,7 +12,6 @@
 ## Timer support for the realtime GC. Based on
 ## `<https://github.com/jckarter/clay/blob/master/compiler/src/hirestimer.cpp>`_
 
-# 
 discard """
 =====================================================
 Nim -- a Compiler for Nim. http://nim-lang.org/
@@ -40,89 +39,7 @@ THE SOFTWARE.
 [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
 """
 
-type
-  Ticks* = distinct int64
-  Nanos* = int64
-{.deprecated: [TTicks: Ticks, TNanos: Nanos].}
-
-when defined(windows):
-
-  proc QueryPerformanceCounter(res: var Ticks) {.
-    importc: "QueryPerformanceCounter", stdcall, dynlib: "kernel32".}
-  proc QueryPerformanceFrequency(res: var int64) {.
-    importc: "QueryPerformanceFrequency", stdcall, dynlib: "kernel32".}
-
-  proc getTicks*(): Ticks {.inline.} =
-    QueryPerformanceCounter(result)
-
-  proc `-`*(a, b: Ticks): Nanos =
-    var frequency: int64
-    QueryPerformanceFrequency(frequency)
-    var performanceCounterRate = 1e+9'f64 / float64(frequency)
-
-    result = Nanos(float64(a.int64 - b.int64) * performanceCounterRate)
-
-elif defined(macosx):
-  type
-    MachTimebaseInfoData {.pure, final, 
-        importc: "mach_timebase_info_data_t", 
-        header: "<mach/mach_time.h>".} = object
-      numer, denom: int32
-  {.deprecated: [TMachTimebaseInfoData: MachTimebaseInfoData].}
-
-  proc mach_absolute_time(): int64 {.importc, header: "<mach/mach.h>".}
-  proc mach_timebase_info(info: var MachTimebaseInfoData) {.importc,
-    header: "<mach/mach_time.h>".}
-
-  proc getTicks*(): Ticks {.inline.} =
-    result = Ticks(mach_absolute_time())
-  
-  var timeBaseInfo: MachTimebaseInfoData
-  mach_timebase_info(timeBaseInfo)
-    
-  proc `-`*(a, b: Ticks): Nanos =
-    result = (a.int64 - b.int64)  * timeBaseInfo.numer div timeBaseInfo.denom
-
-elif defined(posixRealtime):
-  type
-    Clockid {.importc: "clockid_t", header: "<time.h>", final.} = object
-
-    TimeSpec {.importc: "struct timespec", header: "<time.h>", 
-               final, pure.} = object ## struct timespec
-      tv_sec: int  ## Seconds. 
-      tv_nsec: int ## Nanoseconds. 
-  {.deprecated: [TClockid: Clickid, TTimeSpec: TimeSpec].}
-
-  var
-    CLOCK_REALTIME {.importc: "CLOCK_REALTIME", header: "<time.h>".}: Clockid
-
-  proc clock_gettime(clkId: Clockid, tp: var Timespec) {.
-    importc: "clock_gettime", header: "<time.h>".}
-
-  proc getTicks*(): Ticks =
-    var t: Timespec
-    clock_gettime(CLOCK_REALTIME, t)
-    result = Ticks(int64(t.tv_sec) * 1000000000'i64 + int64(t.tv_nsec))
-
-  proc `-`*(a, b: Ticks): Nanos {.borrow.}
-
-else:
-  # fallback Posix implementation:  
-  type
-    Timeval {.importc: "struct timeval", header: "<sys/select.h>", 
-               final, pure.} = object ## struct timeval
-      tv_sec: int  ## Seconds. 
-      tv_usec: int ## Microseconds. 
-  {.deprecated: [Ttimeval: Timeval].}
-  proc posix_gettimeofday(tp: var Timeval, unused: pointer = nil) {.
-    importc: "gettimeofday", header: "<sys/time.h>".}
-
-  proc getTicks*(): Ticks =
-    var t: Timeval
-    posix_gettimeofday(t)
-    result = Ticks(int64(t.tv_sec) * 1000_000_000'i64 + 
-                    int64(t.tv_usec) * 1000'i64)
-  proc `-`*(a, b: Ticks): Nanos {.borrow.}
+include "system/timers"
 
 type clock* = object
   clockStart*: int64
@@ -130,31 +47,29 @@ type clock* = object
 
 proc start*(c: var clock) {.inline.} = c.clockStart = getTicks().Nanos
 proc stop*(c: var clock) {.inline.} = c.clockStop = getTicks().Nanos
-proc seconds*(c: clock): string {.inline.} = $(float64(c.clockStop - c.clockStart)/1000000000) & "s"
+proc nanoseconds*(c: clock): int64 {.inline.} = c.clockStop - c.clockStart
+proc seconds*(c: clock): float {.inline.} = float64(c.clockStop - c.clockStart)/1_000_000_000
 
-import strutils
-
-
-template bench*(c: clock, body: stmt): stmt =
+template bench*(c: clock, body: stmt): stmt {.immediate.} =
   c.start()
   body
   c.stop()
-  
+
 when isMainModule:
   from os import sleep
 
   var c: clock
-  
+
   # example without using the bench abstraction
   c.start()
   sleep(1)
   c.stop()
-  echo ($(c.clockStop - c.clockStart) & "ns") # in nanoseconds
-  echo c.seconds
-  
+  echo c.nanoseconds(), " ns" # in nanoseconds
+  echo c.seconds, " s"
+
   # another example using bench
   var c0, c1, c2: clock
-  
+
   bench(c):
     bench(c0):
       sleep(100)
@@ -164,8 +79,7 @@ when isMainModule:
       for i in 0..<2:
         sleep(1)
 
-  echo "bench 0): ", c0.seconds
-  echo "bench 1): ", c1.seconds
-  echo "bench 2): ", c2.seconds
-  echo "total: ", c.seconds
-
+  echo "bench 0): ", c0.seconds, " s"
+  echo "bench 1): ", c1.seconds, " s"
+  echo "bench 2): ", c2.seconds, " s"
+  echo "total: ", c.seconds, " s"
